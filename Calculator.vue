ebe8175d4bf86430251a8dc2650effeb9a3317d9
dilfamahmood <114667969+dilfamahmood@users.noreply.github.com>
2024-10-31 08:44:30 +0700

    Add files via upload

---
+++ Calculator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="calculator">
+    <input type="text" v-model="num1" placeholder="Masukkan angka pertama">
+    <input type="text" v-model="num2" placeholder="Masukkan angka kedua">
+    <button @click="hitung">Hitung</button>
+    <p>Hasil: {{ hasil }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: '',
+      num2: '',
+      hasil: ''
+    }
+  },
+  methods: {
+    hitung() {
+      this.hasil = parseFloat(this.num1) + parseFloat(this.num2);
+    }
+  }
+}
+</script>
